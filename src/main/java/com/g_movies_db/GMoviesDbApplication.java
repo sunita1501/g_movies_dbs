f42b89368d37aaadc9997dc7dd75e692e6fe736b
sunita1501 <sunita.kumari@cognizant.com>
2021-04-11 23:22:57 -0400

    Second commit

---
+++ src/main/java/com/g_movies_db/GMoviesDbApplication.java
@@ -0,0 +1,13 @@
+package com.g_movies_db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GMoviesDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GMoviesDbApplication.class, args);
+	}
+
+}
